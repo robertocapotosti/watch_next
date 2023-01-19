aa22f10335758e071dc2e811a7bd98eaa901e655
Roberto Capotosti <online@robertocapotosti.it>
2023-01-19 00:16:04 +0000

    first commit

--- watch_next.py
+++ watch_next.py
@@ -1,1 +1,23 @@
 #=== IMPORT SECTION ===
+import spacy
+
+#=== VARIABLE DECLARATIONS ===
+movies = []
+
+#=== FUNCTION SECTION ===
+def load_movies():
+	# variable declarations
+	global movies
+
+	f_movies = open("movies.txt","r",encoding="utf-8")
+	for line in f_movies:
+		fields = line.split(":")
+		fields[0] = fields[0].strip()
+		fields[1] = fields[1].strip()
+		movies.append(fields)
+	f_movies.close()
+
+#=== MAIN SECTION ===
+load_movies()
+for movie in movies:
+	print(movie)
